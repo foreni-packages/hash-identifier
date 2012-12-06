ef9e4b8129c0d743487013603f08af922ffcb07c
Devon Kearns <dookie@kali.org>
2012-12-06 13:26:52 -0700

    Edited debian files

--- debian/hash-identifier.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the hash-identifier package
-#
-0 4	* * *	root	[ -x /usr/bin/hash-identifier_maintenance ] && /usr/bin/hash-identifier_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(hash-identifier):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="hash-identifier" command="/usr/bin/hash-identifier"
